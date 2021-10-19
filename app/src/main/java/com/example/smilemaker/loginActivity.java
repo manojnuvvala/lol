d454e8e695f190ecd41c23db7027366c4b1c0198
saikiranreddy <saikiranreddyenugala@gmail.com>
2021-10-18 20:32:17 -0500

    on click for singup page linked

--- app/src/main/java/com/example/smilemaker/loginActivity.java
+++ app/src/main/java/com/example/smilemaker/loginActivity.java
@@ -1,20 +1,22 @@
 package com.example.smilemaker;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class loginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.registration);
     }
     public void signup (View view){
         Intent intent = new Intent(getBaseContext(),loginActivity.class);
         startActivity(intent);
     }
 }
+
