4f13a2dbe8e9ad74aa531e09c8f0dd47de737008
manojnuvvala <manoj.nuvvala@gmail.com>
2021-10-30 01:09:12 -0500

    Changed some id along with functionality to deploy app

--- app/src/main/java/com/example/smilemaker/homepage.java
+++ app/src/main/java/com/example/smilemaker/homepage.java
@@ -5,13 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class homepage extends AppCompatActivity {
+
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        txt = findViewById(R.id.textView6);
+        txt.setText("A dog is a dog except when he is facing you. Then he is Mr. Dog.");
     }
     public void log (View view){
         Intent intent = new Intent(getBaseContext(),MainActivity.class);
