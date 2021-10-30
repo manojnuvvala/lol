2a690984cf6e5e74402003517e31e5cc53dce0a0
saikiranreddy <saikiranreddyenugala@gmail.com>
2021-10-30 00:32:55 -0500

    Updated main activity to get data used in login page

--- app/src/main/java/com/example/smilemaker/MainActivity.java
+++ app/src/main/java/com/example/smilemaker/MainActivity.java
@@ -5,22 +5,51 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText userName;
+    EditText password;
+
+
+    usermodal um = usermodal.getSingleton();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userName = findViewById(R.id.editTextTextPersonName);
+        password = findViewById(R.id.editTextTextPersonName2);
+
     }
+
+
     public void signup (View view){
+
+
         Intent intent = new Intent(getBaseContext(),loginActivity.class);
         startActivity(intent);
     }
 
     public void signinid(View view){
-        Intent intent1 = new Intent(getBaseContext(),homepage.class);
-        startActivity(intent1);
+
+        String user = userName.getText().toString();
+        String pass = password.getText().toString();
+
+        for(int i = 0; i < um.allUsers.size(); i++) {
+            if(um.allUsers.get(i).login.equals(user) && um.allUsers.get(i).password.equals(pass) ) {
+                Intent intent1 = new Intent(getBaseContext(),homepage.class);
+                startActivity(intent1);
+            }
+        }
+
+
     }
 
 }
